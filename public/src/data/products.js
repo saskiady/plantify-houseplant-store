4ab1ceffe9f9561efdc77dee40a90dc79f130076
saskiady <168164874+saskiady@users.noreply.github.com>
2025-10-22 10:15:56 +0800

    Create products.js

---
+++ public/src/data/products.js
@@ -0,0 +1,47 @@
+// Six unique plants across categories
+const products = [
+  {
+    id: "p1",
+    name: "Monstera Deliciosa",
+    price: 20.0,
+    category: "Tropical",
+    thumbnail: "https://i.imgur.com/J5LVHEL.jpg",
+  },
+  {
+    id: "p2",
+    name: "Snake Plant",
+    price: 15.0,
+    category: "Succulent",
+    thumbnail: "https://i.imgur.com/6r2Qv7L.jpg",
+  },
+  {
+    id: "p3",
+    name: "Fiddle Leaf Fig",
+    price: 40.0,
+    category: "Tree",
+    thumbnail: "https://i.imgur.com/5uCk7JQ.jpg",
+  },
+  {
+    id: "p4",
+    name: "ZZ Plant",
+    price: 18.0,
+    category: "Low-light",
+    thumbnail: "https://i.imgur.com/2xj2J7e.jpg",
+  },
+  {
+    id: "p5",
+    name: "Pothos",
+    price: 12.0,
+    category: "Trailing",
+    thumbnail: "https://i.imgur.com/3QbQ2vA.jpg",
+  },
+  {
+    id: "p6",
+    name: "Aloe Vera",
+    price: 10.0,
+    category: "Succulent",
+    thumbnail: "https://i.imgur.com/1g7R0Yk.jpg",
+  },
+];
+
+export default products;
